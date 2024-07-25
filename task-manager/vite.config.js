97a6d563003354a46fab6f90562f399379c2abf0
JohnCoffeeXL <jagustin.avalos74@gmail.com>
2024-07-25 14:01:31 -0300

    Bug Solved

--- task-manager/vite.config.js
+++ task-manager/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/task-manager/',  
+  base: '/TK-Manager/',  
   plugins: [react()],
 });
