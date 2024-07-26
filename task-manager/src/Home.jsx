09c0da89c77b4579134a61c81ab740e79a132ce3
Juan A. Avalos <128993345+JohnCoffeeXL@users.noreply.github.com>
2024-07-26 17:05:52 -0300

    Update Home.jsx

--- task-manager/src/Home.jsx
+++ task-manager/src/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
     <>
       <div className="location">Home</div>
       <p className='desc'>¡Bienvenido a <b>TK-Manager!</b> <br></br> Aquí podrás hacer listas de tareas personalizadas, editar datos de usuarios randoms dadas por APIs improvisadas y más</p>
-      <img className='welcome' src="./src/IMG/welcome.jpg" alt=""/>
+      <img className='welcome' src="./src/IMG/welcome2.jpg" alt=""/>
     </>
   )
 }
