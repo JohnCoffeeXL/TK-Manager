24b7a00c47d7dc431d12a7f82ee2d43b0a896f59
JohnCoffeeXL <jagustin.avalos74@gmail.com>
2024-07-26 17:03:34 -0300

    Some bugs solved

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TaskList from './TaskList';
 import Home from './Home';
 import Contact from './Contact';
