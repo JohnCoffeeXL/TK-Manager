29be77904623439b5138969ac1eaf6f3202f9ac2
JohnCoffeeXL <jagustin.avalos74@gmail.com>
2024-07-26 17:24:55 -0300

    Image Solved

--- task-manager/src/Home.jsx
+++ task-manager/src/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
     <>
       <div className="location">Home</div>
       <p className='desc'>¡Bienvenido a <b>TK-Manager!</b> <br></br> Aquí podrás hacer listas de tareas personalizadas, editar datos de usuarios randoms dadas por APIs improvisadas y más</p>
-      <img className='welcome' src="./src/IMG/welcome2.jpg" alt=""/>
+      <img className='welcome' src="https://cdn.leonardo.ai/users/61238c17-3841-462e-80f1-386772415698/generations/d28dc468-23f8-4d38-86fc-adfaef484222/Default_A_formal_person_looking_and_welcoming_at_the_camera_fr_1.jpg" alt=""/>
     </>
   )
 }
